5bb868152fc78dccb951fc14f576a0c882c4b526
Safayedbin <safayed.binislam@gmail.com>
2023-05-19 17:05:11 +0600

    module2

--- intro.js
+++ intro.js
@@ -2,19 +2,29 @@
 
 // it is dynamic and need no intialliazation of vatiables
 
+//Printing function
+//alert('Hello learner');
+console.log('Welcome to javascript tutorial')
+
 
 // Variables
 
 //ways to  type variables 
 let a = 10;
 var myString= 'Safayed';
+// Array also have no issues in saving diferent 
+//datatypes in it 
 var myArray= [1, 'strongman', 2.366];
+console.log('Array is printer here\n', myArray);
+
+console.log('\naccesing array like this\n', myArray[1]);
+
+console.log('\nshows array length by this function\n', myArray.length);
 
 
 
-
-// all the datatypes are same as C 
-// new data type Object
+// All the datatypes are same as C 
+// new data type Object in JS
 
 const myObject = {
     key1: 'value1',
