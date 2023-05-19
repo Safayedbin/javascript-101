d605762781992a59146c7af0e650fac9feb232e9
Safayedbin <safayed.binislam@gmail.com>
2023-05-19 10:39:38 +0600

    firstmodule

--- intro.js
+++ intro.js
@@ -1,1 +1,29 @@
-const firstString = 'Hello world';
+//javascript manual 
+
+// it is dynamic and need no intialliazation of vatiables
+
+
+// Variables
+
+//ways to  type variables 
+let a = 10;
+var myString= 'Safayed';
+var myArray= [1, 'strongman', 2.366];
+
+
+
+
+// all the datatypes are same as C 
+// new data type Object
+
+const myObject = {
+    key1: 'value1',
+    key2: 'value2'
+};
+
+//calling key of a object 
+console.log(myObject.key1);
+//Or
+console.log(myObject['key2']);
+
+
